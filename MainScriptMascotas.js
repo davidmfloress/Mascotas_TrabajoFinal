6ee5eb3f2fa306544ea00ac6f490fe900448e615
davidmfloress <davidmf017@gmail.com>
2025-01-28 17:51:09 +0100

    he puesto que salga lo de quitar hambre automatico

--- MainScriptMascotas.js
+++ MainScriptMascotas.js
@@ -41,6 +41,7 @@ window.onload = function () {
     // setInterval(ponerMascota, 500);
     document.getElementById("btnAceptar").addEventListener("click", ponerMascota);
     document.getElementById("btnAceptar").addEventListener("click", function () { setInterval(ponerMascota, 500) });
+    document.getElementById("btnAceptar").addEventListener("click", function(){ setInterval(quitarHambre(), 5000) });
     // document.getElementById("btnAceptar").addEventListener("click", PonerBotones);
 
 
